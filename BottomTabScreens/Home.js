7fdcb83a681271e4dd3b847a2453beffb4fbe8a1
Sakshi Chavre <chavresakshi@gmail.com>
2021-07-05 16:01:20 +0530

    checking

--- BottomTabScreens/Home.js
+++ BottomTabScreens/Home.js
@@ -7,6 +7,7 @@ const Home = (props) => {
   return (
     <Container>
       <Text style={{ color: Colors.accent }}>Home Screen</Text>
+      <Text style={{ color: Colors.accent }}>checking</Text>
     </Container>
   );
 };
