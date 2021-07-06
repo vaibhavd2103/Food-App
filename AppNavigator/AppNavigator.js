e88c1bd3d5d11deacd05d1c9d934a35f6dfeeece
VAIBHAV <68508494+vaibhavd2103@users.noreply.github.com>
2021-07-06 10:39:08 +0530

    updated constants

--- AppNavigator/AppNavigator.js
+++ AppNavigator/AppNavigator.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -12,7 +12,12 @@ import Home from "../BottomTabScreens/Home";
 import Search from "../BottomTabScreens/Search";
 import Cart from "../BottomTabScreens/Cart";
 import { Colors } from "../Constants/Constants";
-import { SimpleLineIcons } from "react-native-vector-icons";
+import {
+  SimpleLineIcons,
+  AntDesign,
+  Ionicons,
+  FontAwesome5,
+} from "react-native-vector-icons";
 
 const Stack = createStackNavigator();
 
@@ -20,9 +25,27 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="BottomTab">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="BottomTab" component={BottomTab} />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="BottomTab"
+          component={BottomTab}
+          options={{
+            headerShown: false,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -30,7 +53,9 @@ const AppNavigator = () => {
 
 const Tab = createBottomTabNavigator();
 
-function BottomTab() {
+function BottomTab(props) {
+  const [selected, setSelected] = useState("");
+
   return (
     <Tab.Navigator
       tabBarOptions={{
@@ -41,7 +66,7 @@ function BottomTab() {
           backgroundColor: Colors.primary,
 
           // Max Height...
-          height: 70,
+          height: 60,
           borderRadius: 10,
           // Shadow...
           elevation: 15,
@@ -49,7 +74,7 @@ function BottomTab() {
           shadowOffset: { width: 5, height: 10 },
           shadowOpacity: 1,
           shadowRadius: 0,
-          paddingHorizontal: 0,
+          paddingHorizontal: 20,
           width: "90%",
           alignSelf: "center",
           position: "absolute",
@@ -63,12 +88,103 @@ function BottomTab() {
         component={Home}
         options={{
           tabBarIcon: () => {
-            return <TouchableOpacity></TouchableOpacity>;
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  props.navigation.navigate("Home");
+                  setSelected("home");
+                }}
+                style={{
+                  position: "relative",
+                  height: selected === "home" ? 60 : 60,
+                  width: selected === "home" ? 60 : 60,
+                  borderRadius: 100,
+                  backgroundColor:
+                    selected === "home" ? Colors.secondary : "transparent",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  bottom: selected === "home" ? 25 : 0,
+                  elevation: selected === "home" ? 10 : 0,
+                }}
+              >
+                {selected === "home" ? (
+                  <Ionicons name="home" size={28} color={Colors.accent} />
+                ) : (
+                  <Ionicons name="home-outline" size={24} color="white" />
+                )}
+              </TouchableOpacity>
+            );
           },
         }}
       />
-      <Tab.Screen name="Search" component={Search} />
-      <Tab.Screen name="Cart" component={Cart} />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: () => {
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  props.navigation.navigate("Search");
+                  setSelected("search");
+                }}
+                style={{
+                  position: "relative",
+                  height: selected === "search" ? 60 : 60,
+                  width: selected === "search" ? 60 : 60,
+                  borderRadius: 100,
+                  backgroundColor:
+                    selected === "search" ? Colors.secondary : "transparent",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  bottom: selected === "search" ? 25 : 0,
+                  elevation: selected === "search" ? 10 : 0,
+                }}
+              >
+                {selected === "search" ? (
+                  <FontAwesome5 name="search" size={24} color={Colors.accent} />
+                ) : (
+                  <Ionicons name="search-outline" size={24} color="white" />
+                )}
+              </TouchableOpacity>
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={Cart}
+        options={{
+          tabBarIcon: () => {
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  props.navigation.navigate("Cart");
+                  setSelected("cart");
+                }}
+                style={{
+                  position: "relative",
+                  height: selected === "cart" ? 60 : 60,
+                  width: selected === "cart" ? 60 : 60,
+                  borderRadius: 100,
+                  backgroundColor:
+                    selected === "cart" ? Colors.secondary : "transparent",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  bottom: selected === "cart" ? 25 : 0,
+                  elevation: selected === "cart" ? 10 : 0,
+                }}
+              >
+                {selected === "cart" ? (
+                  <Ionicons name="cart-sharp" size={28} color={Colors.accent} />
+                ) : (
+                  <Ionicons name="cart-outline" size={24} color="white" />
+                )}
+              </TouchableOpacity>
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
--- Constants/Constants.js
+++ Constants/Constants.js
@@ -9,3 +9,17 @@ export const Colors = {
   subtitle: "#a1a1a1",
   black: "black",
 };
+
+export const Font = {
+  header: 18,
+  title: 14,
+  subtitle: 12,
+  mini: 9,
+};
+
+export const Sizes = {
+  padding: 10,
+  margin: 15,
+  height: "100%",
+  width: "100%",
+};
