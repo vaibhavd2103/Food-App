7372fd1e238386069da907c35a0cadcacc24373e
VAIBHAV <68508494+vaibhavd2103@users.noreply.github.com>
2021-07-06 21:25:49 +0530

    Updated color scheme a bit

--- BottomTabScreens/Home.js
+++ BottomTabScreens/Home.js
@@ -7,18 +7,22 @@ import {
   Dimensions,
 } from "react-native";
 import { Avatar } from "react-native-paper";
-import { Container , Header} from "../Components/Components";
+import { Container, Header } from "../Components/Components";
 import { Colors, Font, Sizes } from "../Constants/Constants";
 import { StatusBar } from "expo-status-bar";
-import HomeCarousel from '../Components/HomeComponents/HomeCarousel';
-
+import HomeCarousel from "../Components/HomeComponents/HomeCarousel";
 
 const Home = (props) => {
   return (
     <Container style={{ paddingTop: Sizes.padding * 4 }}>
       <StatusBar style="dark" />
-      <Header style={{ justifyContent: "space-between", paddingHorizontal:Sizes.padding }}>
-        <Text style={{ ...Font.Header }}>Hungerezi</Text>
+      <Header
+        style={{
+          justifyContent: "space-between",
+          paddingHorizontal: Sizes.padding,
+        }}
+      >
+        <Text style={{ ...Font.header }}>Hungrezi</Text>
         <Avatar.Image
           source={{
             uri: "https://i.pinimg.com/474x/75/66/57/756657573134baeb9cdbe188195967e7--sexy-men-hot-men.jpg",
@@ -28,14 +32,10 @@ const Home = (props) => {
       </Header>
 
       <HomeCarousel navigation={props.navigation} />
-      
-      
     </Container>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({
-  
-});
+const styles = StyleSheet.create({});
